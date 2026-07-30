239be99d5906a0c7f7a202b293abb72dace5f283
Ilya Panfilov <ipanfilo@amd.com>
2026-07-30 19:12:48 -0400

    Fix build. Add fwd workspace calculation API

--- qola/cpp_itfs/qola_mha_fwd.cu
+++ qola/cpp_itfs/qola_mha_fwd.cu
@@ -12,9 +12,16 @@ float mha_fwd(const aiter::mha_fwd_args& args, const ck_tile::stream_config& str
     return ::aiter::mha_fwd(args, stream_config);
 }
 
+#if FAV_NATIVE_ON
 int mha_fwd_calculate_num_splits(const aiter::mha_fwd_args& args)
 {
     return ::aiter::mha_fwd_calculate_num_splits(args);
 }
+
+size_t mha_fwd_workspace_size(const aiter::mha_fwd_args& a)
+{
+    return ::aiter::mha_fwd_workspace_size(a);
+}
+#endif
 
 QOLA_NS_END
